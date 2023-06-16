4178d8aed556a0608d80fbfdc3f697af0656e883
inbn6619 <inbn6619@gmail.com>
2023-06-16 21:15:54 +0900

    study

--- 코딩테스트 고득점/깊이 너비 우선 탐색/게임 맵 최단거리.py
+++ 코딩테스트 고득점/깊이 너비 우선 탐색/게임 맵 최단거리.py
@@ -1,15 +1,36 @@
 def solution(maps):
 
-    width = len(maps)
-    height = len(maps[0])
+    height = len(maps)
+    width = len(maps[0])
 
     answer = 0
     dx = [1, 0, -1, 0]
     dy = [0, 1, 0, -1]
     board = [[0] * width for _ in range(height)]
     
-    def bfs(x, y):
+    def bfs(start):
+        from collections import deque
+        queue = deque([start])
+        board[start[1]][start[0]] = 1
 
+        while queue:
+            x, y = queue.popleft()
+            
+            for i in range(4):
+                nx = dx[i] + x
+                ny = dy[i] + y
+
+                if 0 <= nx < width and 0 <= ny < height and maps[ny][nx] == 1 and board[ny][nx] == 0:
+                    queue.append([nx, ny])
+                    board[ny][nx] = board[y][x] + 1
+                    maps[ny][nx] = 0
+
+        if board[height-1][width-1] == 0:
+            return -1
+        else:
+            return board[height-1][width-1]
+
+    answer = bfs([0, 0])
 
     return answer
 
