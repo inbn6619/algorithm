0245244c671a43a74baac27f4763a84dd41123ed
inbn6619 <inbn6619@gmail.com>
2023-06-19 22:39:25 +0900

    study

---
+++ 코딩테스트 고득점/깊이 너비 우선 탐색/단어 변환.py
@@ -0,0 +1,9 @@
+def solution(begin, target, words):
+    answer = 0
+    return answer
+
+words1 = ["hot", "dot", "dog", "lot", "log", "cog"]
+words2 = ["hot", "dot", "dog", "lot", "log"]
+
+print(solution("hit", "cog", words1)) # 4
+print(solution("hit", "cog", words2)) # 0
