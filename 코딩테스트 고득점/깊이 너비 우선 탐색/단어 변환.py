550f787eee5f6ebf2a7b87eefe833e55d6baf03d
inbn6619 <inbn6619@gmail.com>
2023-06-19 23:04:44 +0900

    study 단어변환

--- 코딩테스트 고득점/깊이 너비 우선 탐색/단어 변환.py
+++ 코딩테스트 고득점/깊이 너비 우선 탐색/단어 변환.py
@@ -1,5 +1,42 @@
 def solution(begin, target, words):
+ 
+    visited = list()
     answer = 0
+    
+    def check(a, b):
+        n = len(a)
+        cnt = 0
+
+        for i in range(n):
+            if a[i] == b[i]:
+                cnt += 1
+        
+        if cnt + 1 == n:
+            return True
+        return False
+
+
+    def dfs(begin, target, node):
+        visited.append(node)
+
+        if target == node:
+            nonlocal answer
+            answer = len(visited)
+            return
+
+        for word in words:
+            if word not in visited and check(node, word):
+                dfs(node, target, word)
+                visited.pop()
+
+
+    for w in words:
+        if check(begin, w):
+            dfs(begin, target, w)
+
+
+
+    
     return answer
 
 words1 = ["hot", "dot", "dog", "lot", "log", "cog"]
