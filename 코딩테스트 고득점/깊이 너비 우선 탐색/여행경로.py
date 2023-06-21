f5499e2921485183c282a28883faa83deae6b400
inbn6619 <inbn6619@gmail.com>
2023-06-21 20:48:51 +0900

    여행경로 100점

--- 코딩테스트 고득점/깊이 너비 우선 탐색/여행경로.py
+++ 코딩테스트 고득점/깊이 너비 우선 탐색/여행경로.py
@@ -1,39 +1,58 @@
 def solution(tickets):
     result = list()
 
-
     def dfs(end):
         nonlocal cnt
-        path.append(end)
-
-        for i in range(len(tickets)):
-            if tickets[i][0] == end and i not in idx:
-                cnt += 1
-                idx.append(i)
-                dfs(tickets[i][1])
-
+        nonlocal check
         if cnt == len(tickets):
-            return
-        
-        path.pop()
-        idx.pop()
-                
-    for n in range(len(tickets)):
-        if tickets[n][0] =='ICN':
-            idx = [n]
-            path = [tickets[n][0]]
-            cnt = 1
-            dfs(tickets[n][1])
             result.append(path.copy())
-    
-    result.sort()
-    answer = result[0]
+            return
+
+        for n in range(len(tickets)):
+            if n not in idx and end == tickets[n][0]:
+                path.append(tickets[n][1])
+                idx.append(n)
+                cnt += 1
+                dfs(tickets[n][1])
+                cnt -= 1
+                path.pop()
+                idx.pop()
+        
+        if check:
+            result.append(path.copy())
+            check = False
+
+    for i in range(len(tickets)):
+        if tickets[i][0] == 'ICN':
+            cnt = 1
+            path = tickets[i].copy()
+            idx = [i]
+            check = True
+            dfs(tickets[i][1])
+
+
+    Max_num = 0
+    checklist = list()
+    for lst in result:
+        if len(lst) >= Max_num:
+            Max_num = len(lst)
+
+    for lst in result:
+        if len(lst) == Max_num:
+            checklist.append(lst)
+
+    checklist.sort()
+    answer = checklist[0]
     return answer
-
-
 
 ticket1 = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
 ticket2 = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+ticket3 = [['ICN', "ASD"], ["ASD", "DFS"]]
+ticket4 = [['ICN', "ASD"], ["GDS", "DFS"]]
+
+
 
 print(solution(ticket1))
 print(solution(ticket2))
+print(solution(ticket3))
+print(solution(ticket4))
