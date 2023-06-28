83564ac273e30dd6bd550ef16ff94816937413b4
inbn6619 <inbn6619@gmail.com>
2023-06-28 17:09:28 +0900

    retry

--- 코딩테스트 고득점/깊이 너비 우선 탐색/퍼즐 조각 채우기.py
+++ 코딩테스트 고득점/깊이 너비 우선 탐색/퍼즐 조각 채우기.py
@@ -20,7 +20,7 @@ def solution(game_board, table):
     table_R = rotate(table)
     table_L = rotate(rotate(table_R))
 
-    def bfs(start, board, T, F):
+    def bfs(start, board, T, F, n, m):
         queue = deque([start])
         coor = list()
         coor.append(start)
@@ -31,7 +31,7 @@ def solution(game_board, table):
                 nx = dx[i] + x
                 ny = dy[i] + y
 
-                if 0 <= nx < n and 0 <= ny < n and board[ny][nx] == T:
+                if 0 <= nx < m and 0 <= ny < n and board[ny][nx] == T:
                     coor.append([ny, nx])
                     queue.append([ny, nx])
                     board[ny][nx] = F
@@ -44,7 +44,7 @@ def solution(game_board, table):
             for j in range(n):
                 if tab[i][j] == 1:
                     tab[i][j] = 0
-                    coors.append(bfs([i, j], tab, 1, 0))
+                    coors.append(bfs([i, j], tab, 1, 0, len(tab), len(tab[0])))
                     
         table_coor.append(coors)
 
@@ -53,7 +53,7 @@ def solution(game_board, table):
         for b in range(n):
             if game_board[a][b] == 0:
                 game_board[a][b] = 1
-                board_coor.append(bfs([a, b], game_board, 0, 1))
+                board_coor.append(bfs([a, b], game_board, 0, 1, len(game_board), len(game_board[0])))
 
     def func(array):
         y, x = list(), list()
@@ -87,25 +87,65 @@ def solution(game_board, table):
             else:
                 table_square[tab][table_square[0].index(rotate(rotate(rotate(square))))] = rotate(rotate(rotate(square)))
 
-    checklist = [[False] * len(board_square)]
+    board_check = [False] * len(board_square)
+    table_check = [False] * len(table_square[0])
 
-    # def dfs(arr1, arr2, num):
-    #     n = len(arr1)
-    #     m = len(arr1[0])
+    def dfs(board, table, num, visited):
+        for i in range(len(table)):
+            if not table_check[i]:
+                target = board.copy()
+                array = table.copy()
+                for a in range(len(array[i])):
+                    for b in range(len(array[i][0])):
+                        if array[i][a][b] == 1:
+                            stack = [bfs([a,b], array[i], 1, 0, len(array[i]), len(array[i][0]))[0]]
+                visited.append(i)
+                Check = True
 
-    #     for i in range(n):
-    #         for j in range(m):
+                while stack:
+                    y, x = stack.pop()
+                
+                    for j in range(4):
+                        nx = dx[j] + x
+                        ny = dy[j] + y
+
+                        if 0 <= nx < len(array[i][0]) and 0 <= ny < len(array[i]):
+                            if (target[ny][nx] == 1 and array[i][ny][nx] == 1):
+                                target[ny][nx] = 0
+                                array[i][ny][nx] = 0
+                            elif target[ny][nx] == 1 or array[i][ny][nx] == 1:
+                                Check = False
+                                visited.pop()
+                                break
+                if Check:
+                    # 여기서 target 안의 1 좌료 추출 후 해당 좌표로 func 돌려서 target 갱신해주고
+                    new_board = list()
+                    for i in range(len(target)):
+                        for j in range(len(target[0])):
+                            if target[i][j] == 1:
+                                new_board = func(bfs([i,j], target, 1, 0, len(target), len(target[0])))
+                                
+                    
+
+                    # 갱신된 값을 가지고 dfs 한번 들어가주고 target이 전부 0일 경우 checklist[i]에 visited 사용해서 갱신해주면됨
+
+                    
                 
 
-        
 
+
+    visited = list()
+    
     for i in range(len(board_square)):
-        for j in range(len(table_square)):
-            a, b, c, d = len(board_square[0]), len(board_square[0][0]), len(table_square[0]), len(table_square[0][0])
-            if a >= c and b >= d:
-                if board_square[i] == table_square[i]:
-                    checklist[i] = True
-                
+        for square in table_square:
+            for j in range(len(square)):
+                if not board_check[i] and not table_check[j]:
+                    if board_square[i] == square[j]:
+                        board_check[i] = True
+                        table_check[j] = True
+                    else:
+                        dfs(board_square[i], square, i, visited)
+
 
 
 
