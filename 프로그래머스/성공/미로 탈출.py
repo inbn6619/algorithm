47b09682c9a650e12428943d10bfa506595e4869
inbn6619 <inbn6619@gmail.com>
2024-02-06 17:50:49 +0900

    Escape from the maze : Fixed version

--- 프로그래머스/성공/미로 탈출.py
+++ 프로그래머스/성공/미로 탈출.py
@@ -41,49 +41,6 @@ def solution(maps):
                 
                 if 0 <= nx < w and 0 <= ny < h:
                     if visited[ny][nx] == 0 and graph[ny][nx] != "X":
-                 from collections import deque
-
-def solution(maps):
-    answer = 0
-    
-    matrix = [list(i) for i in maps]
-    
-    # print(matrix)
-    
-    h = len(matrix)
-    w = len(matrix[0])
-    
-    for i in range(h):
-        for j in range(w):
-            if matrix[i][j] == "L":
-                lever = [i,j]
-            elif matrix[i][j] == "S":
-                start = [i,j]
-            elif matrix[i][j] == "E":
-                end = [i,j]
-                
-    print(start, lever, end)
-    
-    def bfs(start, graph, target):
-        
-        deq = deque([start])
-                
-        visited = [[0] * w for _ in range(h)]
-        
-        visited[start[0]][start[1]] = 1
-        
-        dx = [1, 0, -1, 0]
-        dy = [0, 1, 0, -1]
-        
-        while deq:
-            y, x = deq.popleft()
-        
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                
-                if 0 <= nx < w and 0 <= ny < h:
-                    if visited[ny][nx] == 0 and graph[ny][nx] != "X":
                         visited[ny][nx] = visited[y][x] + 1
                         deq.append([ny, nx])
 
