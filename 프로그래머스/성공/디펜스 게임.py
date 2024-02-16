42b223bb54a1914e8417d393fc6cf313b8ca2330
inbn6619 <inbn6619@gmail.com>
2024-02-16 11:08:43 +0900

    defense game

---
+++ 프로그래머스/성공/디펜스 게임.py
@@ -0,0 +1,14 @@
+from heapq import heappush, heappop
+
+def solution(n, k, enemy):
+    heap = []
+    
+    for idx, e in enumerate(enemy):
+        heappush(heap, e)
+        
+        if len(heap) > k:
+            n -= heappop(heap)
+        if n < 0:
+            return idx
+            
+    return len(enemy)
