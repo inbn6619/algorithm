b1112d35165ea6eefb7db6e0ecbdaf264e43eda3
inbn6619 <inbn6619@gmail.com>
2024-02-23 02:28:18 +0900

    integer pairs between two circles

---
+++ 프로그래머스/성공/두 원 사이의 정수 쌍.py
@@ -0,0 +1,17 @@
+import math
+
+def solution(r1, r2):
+    answer = 0
+    
+    target1 = r1 ** 2
+    target2 = r2 ** 2
+    
+    for i in range(1, r2+1):
+        maxY = math.floor(math.sqrt(target2 - i**2))
+        minY = math.ceil(math.sqrt(target1 - i**2)) if r1 > i else 0
+        answer += maxY-minY + 1
+        # print(maxY, minY)
+    
+    answer *= 4
+    
+    return answer
